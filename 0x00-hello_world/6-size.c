68fea8f9b4516135fb46a8d772c24366fe666888
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-16 14:33:44 -0800

    Corrected lines

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-	printf("Size of char: %d bytes.\n", sizeof(char));
-	printf("Size of Integer(int): %d bytes.\n", sizeof(int));
-	printf("Size of Long Int: %d bytes.\n", sizeof(long int));
-	printf("Size of Long Long Int: %d bytes.\n", sizeof(long long int));
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
