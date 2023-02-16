b38c340a71bce43591866fe1af6bb09afd3daeba
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-16 14:29:01 -0800

    Corrected space

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 	printf("Size of char: %d bytes.\n", sizeof(char));
-        printf("Size of Integer(int): %d bytes.\n", sizeof(int));
-        printf("Size of Long Int: %d bytes.\n", sizeof(long int));
-        printf("Size of Long Long Int: %d bytes.\n", sizeof(long long int));
-        printf("Size of a float: %d byte(s)\n", sizeof(float));
-        return (0);
+	printf("Size of Integer(int): %d bytes.\n", sizeof(int));
+	printf("Size of Long Int: %d bytes.\n", sizeof(long int));
+	printf("Size of Long Long Int: %d bytes.\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
