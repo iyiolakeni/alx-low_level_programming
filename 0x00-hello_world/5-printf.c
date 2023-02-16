2342dacb632c4e15b84dfd2df9be85506e74456d
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-16 13:41:56 -0800

    A c program to use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - point of entry
+ * Return: 0 always
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
