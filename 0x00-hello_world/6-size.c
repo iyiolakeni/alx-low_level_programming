faf98585595ccce90c5c6cc811f2278f7f7c83d1
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-16 14:18:41 -0800

    Removed Long double

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@ int main(void)
 	printf("Size of Integer(int): %d bytes.\n", sizeof(int));
 	printf("Size of float: %d bytes.\n", sizeof(float));
 	printf("Size of double: %d bytes.\n", sizeof(double));
-	printf("Size of long double: %d  bytes.\n", sizeof(long double));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	return (0);
 }
