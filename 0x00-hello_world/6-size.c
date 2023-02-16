78b9d2bb8deb6bf56b2069454283119a1f6cfd67
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-16 14:23:52 -0800

    Change the data types to char, int, long int, long long int and float

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 	printf("Size of char: %d bytes.\n", sizeof(char));
-	printf("Size of Integer(int): %d bytes.\n", sizeof(int));
-	printf("Size of float: %d bytes.\n", sizeof(float));
-	printf("Size of double: %d bytes.\n", sizeof(double));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	return (0);
+        printf("Size of Integer(int): %d bytes.\n", sizeof(int));
+        printf("Size of Long Int: %d bytes.\n", sizeof(long int));
+        printf("Size of Long Long Int: %d bytes.\n", sizeof(long long int));
+        printf("Size of a float: %d byte(s)\n", sizeof(float));
+        return (0);
 }
