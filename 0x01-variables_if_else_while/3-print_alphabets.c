d58bd2411607325e2f9c654e5c4521fbcd1f9807
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-17 09:14:24 -0800

    A nested for loop

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,9 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
 		putchar(ch);
-		putchar(toupper(ch));
-		putchar('\n');
-	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
 	return (0);
 }
