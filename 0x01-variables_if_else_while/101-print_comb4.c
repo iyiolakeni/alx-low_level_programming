a736fafac6e4d1852b49ebfb481f67aa6c751ebf
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-17 12:16:43 -0800

    3 digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main- 3 different digits combination
+ * Return: 0 always
+*/
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (b = (c + 1); b < 10; b++)
+		{
+			for (a = (b + 1); a < 10; a++)
+			{
+				putchar('0' + c);
+				putchar('0' + b);
+				putchar('0' + a);
+				if (c != 7 || b != 8 a != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
