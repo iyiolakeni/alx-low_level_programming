14390d42bf897d2d21bbcbc6b5b751d5dd146d0d
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-17 08:52:55 -0800

    Added lstln declaration

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastln = n % 10;
+	int lastln = n % 10;
+
 	if (lastln > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastln);
