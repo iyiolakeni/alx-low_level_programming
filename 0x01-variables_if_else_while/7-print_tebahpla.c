fe7d1b927e4dd66083421b02c3e2844322e5798e
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-17 11:11:02 -0800

    Removed white spaces

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- print alphabet in reversed using putchar
+ * Return: 0 always
+*/
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch <= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n);
+	return (0);
+}
