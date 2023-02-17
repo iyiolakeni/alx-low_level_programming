e5f6bfab626cf31c0d5286373cc15627501f22fb
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-17 09:22:47 -0800

    A blank line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-the entry point
+ * Return:0 always
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			goto;
+		}
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
