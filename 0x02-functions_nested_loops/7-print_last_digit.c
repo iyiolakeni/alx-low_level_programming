e234a412e2af11a8c587d34ffdbba98bb0046f26
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-21 11:46:15 -0800

    First try

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit- prints the last digit of a number
+ * @n: value to be modulated
+ * Return: value of the last digit
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
