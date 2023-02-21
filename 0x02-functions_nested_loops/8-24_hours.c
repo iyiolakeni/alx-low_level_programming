a8cb98281c76c040f26cb4057dd03db09acac5b1
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-21 12:09:00 -0800

    Print time and sec

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer- print every minute of the day
+ *
+*/
+void jack_bauer(void)
+{
+	int min;
+	int sec;
+
+	for (min = 0; min <=23; min++)
+	{
+		for (sec = 0;sec <=59; sec++)
+		{
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar(':');
+			_putchar(sec / 10 + '0');
+			_putchar(sec % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
