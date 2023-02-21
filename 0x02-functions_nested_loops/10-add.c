a63f54cd9a3b30568a4d34dc6f9b59e939d36855
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-21 12:18:24 -0800

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add- add two integers
+ * @n: number to be added
+ * @x: number to be added
+ * Return: addition
+*/
+int add(int n, int x)
+{
+	_putchar(n + x);
+	return (n + x);
+}
