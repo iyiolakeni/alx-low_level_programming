49da520461439310513cf41fa72d47d1de7be90f
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-21 10:35:00 -0800

    Print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- prints 10 times the alphabet in lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int a = 0;
+	char n;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
