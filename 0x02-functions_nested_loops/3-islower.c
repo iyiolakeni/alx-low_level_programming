2646479d6859440242f57df0e983d0ad033b7b28
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-21 10:46:30 -0800

    Check for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower- checks for lowercase
+ * Return: 0 or 1 as success
+ * @c: input received
+*/
+int _islower(int c)
+{
+	char ch;
+
+	if (islower(ch) != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
