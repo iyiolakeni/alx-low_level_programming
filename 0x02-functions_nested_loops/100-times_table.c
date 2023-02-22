1fa447f898e76fefd8a874d3b5c335f8289e9331
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-22 13:28:03 -0800

    Corrected Syntax

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_times_table(int n)
 	int i;
 	int j;
 
-	if (n !> 15 && n!< 0)
+	if (n !> 15 && n !< 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -18,7 +18,7 @@ void print_times_table(int n)
 
 				if (j == 0)
 				{
-					_putchar('0')
+					_putchar('0');
 				}
 				else if (times <= 9)
 				{
