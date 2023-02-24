3c32f4dd1495013139d0ce2fc75c977c9daba517
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-23 20:52:55 -0800

    commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -74,4 +74,13 @@ void print_square(int size);
  * Return: 0
  */
 void print_triangle(int size);
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
 #endif
