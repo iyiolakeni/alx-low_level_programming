275d0112f8a15c513734f23fb1535170e9bec99c
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-23 20:00:46 -0800

    Multiply

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit- print if it is a digit
+ * @c: number
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul- multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int multiply = a * b;
+
+	return (0);
+}
