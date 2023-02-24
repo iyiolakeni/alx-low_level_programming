85146975256835aca11a8c85c7486b5edf01cc4f
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-23 19:49:52 -0800

    Print upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper- print uppercase
+ * @c: letter
+ * Return: 1 or 0
+*/
+
+int _isupper(int c)
+{
+	char ch;
+
+	if (isupper(ch))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,77 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _isupper- a prototype to check for uppercase
+ * @c: letter to be checked
+ *
+ * Return: 1 as success, 0 as failure
+*/
+int _isupper(int c);
+
+/**
+ * _isdigit- check if it is digit
+ *
+ * @c: number to be checked
+ *
+ * Return: 1 as success, 0 as failure
+*/
+int _isdigit(int c);
+
+/**
+ * mul- to multiply two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: 0 always
+*/
+int mul(int a, int b);
+
+/**
+ * print_numbers- print number 1 to 9
+ *
+ */
+void print_numbers(void);
+
+/**
+ * print_most_numbers- print 1 to 9 followed by a new line except 2 and 4
+ *
+ */
+void print_most_numbers(void);
+
+/**
+ * more_numbers- print 10 times of 0 to 14
+ *
+ */
+void more_numbers(void);
+
+/**
+ * print_line- print line
+ *
+ * @n:number
+ * Return: 0 always
+ */
+void print_line(int n);
+
+/**
+ * print_diagonal- print a diagonal line
+ *
+ * @n: number
+ * Return: 0 always
+ */
+void print_diagonal(int n);
+
+/**
+ * print_square- print square
+ * @size: number
+ * Return: 0 always
+ */
+void print_square(int size);
+
+/**
+ * print_triangle- print triangle
+ * @size: number
+ * Return: 0
+ */
+void print_triangle(int size);
+#endif
