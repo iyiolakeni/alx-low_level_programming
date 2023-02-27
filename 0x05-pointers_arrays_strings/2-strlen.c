bf4418eff9fec7ee90a5689afe9b5ddd1b9b9579
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-27 06:28:39 -0800

    Count string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+* _strlen- update the value it points to
+* @s: letter to be checked
+*
+* Return: 0 always
+*/
+int _strlen(char *s)
+{
+	int lenght;
+
+	lenght = strlen(s);
+}
