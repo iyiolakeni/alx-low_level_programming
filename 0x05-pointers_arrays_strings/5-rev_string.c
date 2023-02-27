39add6472ce4cbcc889b7ce4aecb419b93ea4d8f
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-27 07:02:01 -0800

    Used strrev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+* print_rev- reverse string
+* @s: letter to be checked
+*
+* Return: 0 always
+*/
+void rev_string(char *s)
+{
+	s = strrev(s);
+}
