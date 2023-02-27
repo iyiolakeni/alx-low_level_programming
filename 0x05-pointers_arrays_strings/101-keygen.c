52bcad367e469940fba8f160f27570130d841f40
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-27 15:10:04 -0800

    another try

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -18,7 +18,7 @@ void randomPasswordGeneration(int N)
     srand((unsigned int)(time(NULL)));
     */
     /* Array of numbers */
-    char numbers[] = {"0", "1", "2", "3" ,"4", "5", "6", "7","8","9";
+    char numbers[] = "0123456789";
   
     /* Array of small alphabets */
     char letter[] = "abcdefghijklmnoqprstuvwyzx";
