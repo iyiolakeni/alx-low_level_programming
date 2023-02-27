f4dee4185554deffce81c7fa381b62d6ef8d2533
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-27 10:22:46 -0800

    Print other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.h"
+/**
+* puts_half- prints half of a string
+* @str: letter to be checked
+*
+*
+*/
+void puts_half(char *str)
+{
+	int i;
+	int n;
+
+	if (_strlen(str) % 2 != 0)
+	{
+		n = (_strlen(str) - 1) / 2;
+	}
+	for (i = n; i < _strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
