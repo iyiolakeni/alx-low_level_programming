d688e839fb212a4885db17795ac77173c93ebe7c
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-27 06:58:36 -0800

    Added Betty coding style

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,9 @@ void print_rev(char *s)
 {
 	int i, n = strlen(s);
 
-	for (i = n -1; i >= 0; i--)
+	for (i = n - 1; i >= 0; i--)
 	{
-		_putchar(*(s+i));
+		_putchar(*(s + i));
 	}
 	_putchar('\n');
 }
