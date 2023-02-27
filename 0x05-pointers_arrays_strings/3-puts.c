b8d461daa4e28d00a6126449e82a937ca172dcf3
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-27 06:38:18 -0800

    Using puts to print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _puts- update the value it points to
+* @str: letter to be checked
+*
+* Return: 0 always
+*/
+int _puts(char *str)
+{
+	puts(str);
+	puts("\n");
+	return (0);
+}
