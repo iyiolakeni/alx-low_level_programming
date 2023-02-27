98875095ad1172b9898b7847733986a8d8949563
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-27 14:37:25 -0800

    Hope this works

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,77 +1,42 @@
+/*
+ *
+ *  gen-passwd.c: generate a random password
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <time.h>
 
-/** randomPassword- function to generate random
- * password of length
- * @N: number to generate
- */
-void randomPassword(int n)
+#define MIN_CHAR 8
+#define MAX_CHAR 1023
+#define BUF_SIZE 1024
+#define PRIME_MOD 937
+
+int main (int argc, char **argv)
 {
-	/* Counters declaration and initialization */
-	int i = 0;
-	int randomizer = 0;
+    int N;
+    char password [BUF_SIZE];
 
-	/* Seed random numbers with currernt time
-	 * so that it changes in time
-	 */
-	srand((unsigned int)(time(NULL)));
+    while (1) {
+        // get N
+        while (1) {
+            printf ("Enter number of digits: ");
+            scanf ("%d", &N);
+            if (!N) exit (EXIT_SUCCESS);
+            if (N >= MIN_CHAR && N <= MAX_CHAR)
+                break;
+            printf ("Password should be between %d - %d characters\n", MIN_CHAR, MAX_CHAR); 
+        }
+    
+        time_t now = time (NULL);
 
-	/* Create an Array of numbers */
-	char num[] = {1,2,3,4,5,6,7,8,9,0};
+        srand ((unsigned int) (now % PRIME_MOD));
 
-	/* Arrays of alphabets */
-	char let[] = "abcdefghijklmonpqrstuvwxyz";
-	char letup[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < N; i++) 
+            password [i] = 33 + rand () % 94;
+    
+        password [N] = '\0';
 
-	/* Array of special symbols */
-	char sym[] = "!@#$%^&*()_+?:{}][|";
-
-	/* Store random passowrd */
-	char password[n];
-
-	/* Select randomizer inside loop */
-	randomizer = rand() % 4;
-
-	for (i = 0; i < n; i++)
-	{
-		if (randomizer == 1)
-		{
-			password[i] = num[rand() % 10];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 2)
-		{
-			password[i] = sym[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 3)
-		{
-			password[i] = letup[rand() % 276];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else
-		{
-			password[i] = let[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-	}
-}
-
-/**
- * main- main program
- * Return 0 always
- */
-int main()
-{
-	int N = 10;
-
-	randomPassword(N);
-
-	return (0);
+        printf ("password = %s\n", password);
+    }
 }
