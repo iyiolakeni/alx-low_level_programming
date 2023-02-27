6557902fa4dac697ad7b7897472f55a2884c29d6
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-02-27 06:50:12 -0800

    Print in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse a string
+ *
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+{
+	int i, n = strlen(s);
+
+	for (i = n -1; i >= 0; i--)
+	{
+		_putchar(*(s+i));
+	}
+	_putchar('\n');
+}
