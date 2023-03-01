ca2e92a1ea3c7b9fd733785403b20aff938e8cc5
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-01 09:39:52 -0800

    Concatenate using n bytes from src

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncat- concatenates two strings
+ * @dest: destination
+ * @src: source
+ * @n: interger
+ * Return: 0 always
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j = strlen(dest); /* determine the length of destination */
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i]; /* increase the dest as i increases */
+	}
+	dest[j + i] = '\0';
+
+	return (dest);
+}
