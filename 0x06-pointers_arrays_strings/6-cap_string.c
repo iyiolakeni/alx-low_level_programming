b7fbb790e52ba53ccab03a68f2d11078d7494d58
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-01 13:00:43 -0800

    Using ASCII VaLues

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,30 @@
 #include "main.h"
 /**
- * seperator- aray of special character
- * @a: array
- * Return: 0 or 1
- */
-int seperator(char a)
+  * cap_string - ...
+  * @s: ...
+  *
+  * Return: char value
+  */
+char *cap_string(char *s)
 {
-	int i;
-	char array[13] = {'\n', '\t', ' ', '.', ',', ';', ',',
-	'!', '?', '(', ')', '{', '}'};
+	int a = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; i < 13; i++)
+	while (s[a])
 	{
-		if (array[i] == a)
-			return (1);
-	}
-	return (0);
-}
-/**
- * cap_string - capitalize all letters in string
- * @ch: string to manipulate
- * Return: string with all letters capitalized
- */
-char *cap_string(char *ch)
-{
-	int i;
+		i = 0;
 
-	for (i = 0; ch[i] != '\0'; i++)
-	{
-		if (seperator(ch[i]))
-			continue;
-		if (ch[i] >= 98 && ch[i] <= 122 && (seperator(ch[i - 1])))
-			ch[i] = ch[i] - 32;
+		while (i < cspc)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+
+			i++;
+		}
+
+		a++;
 	}
-	return (ch);
+
+	return (s);
 }
