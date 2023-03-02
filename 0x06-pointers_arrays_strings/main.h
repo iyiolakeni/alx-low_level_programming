74a5ad40cefc9022ae35b0cb9cfd4e8d6fef4079
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-02 05:43:24 -0800

    Print in buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,5 @@ char *_strcat(char *dest, char *src);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *rot13(char *);
+void print_buffer(char *b, int size);
 #endif
