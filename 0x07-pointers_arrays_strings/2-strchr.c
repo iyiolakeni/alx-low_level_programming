8e6719685ee89dab3d5ac95d85c6c7b8827da7e8
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-06 09:13:50 -0800

    USing strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strchr - function to copy memory area
+ * @s: destination
+ * @c: source
+ * Return: c pointer or null
+*/
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
+
