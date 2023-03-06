cbcd789dc294738ee649cd6134a321edc94f9bf0
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-06 09:57:29 -0800

    Print diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,21 +2,27 @@
 #include "main.h"
 
 /**
- * print_diagsums- print diagonal sum of arrays
+ * print_diagsums- print sum od two diagonals of a square integer
  * @a: array
- * @size: pointer
- * Return: Pointer to the beginning or NULL
+ * @size: size of the array
+ * Return: 0
 */
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int diagonalone = 0;
-	int diagonaltwo = 0;
+	int sum0, sum1;
+	int row, col;
 
-	for (i = 0; i < size; i++)
+	sum0 = 0;
+	sum1 = 0;
+	row = 0;
+	col = 0;
+
+	while (row < size)
 	{
-		diagonalone += a[(i * size) + 1];
-		diagonaltwo += a[(size - 1) + ((size - 1) * i)];
+		sum0 += *(a + row  * size + col);
+		sum1 += *(a + row  * size + (size - 1 - col));
+		col++;
+		row++;
 	}
-	printf("%d, %d\n", diagonalone, diagonaltwo);
+	printf("%d, %d\n", sum0, sum1);
 }
