a972068f7add329c6ac2714744f791a1b4aad4e8
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-10 12:31:05 -0800

    Using attribute unused

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+*/
+int main(int argc __attribute__ ((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
