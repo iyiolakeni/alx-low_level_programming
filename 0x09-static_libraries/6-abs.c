3cd9c1f6d3ed791d6bff88177643832e5b8982d5
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-10 11:04:03 -0800

    A new try

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,13 +1,16 @@
 #include "main.h"
-#include <stdlib.h>
 /**
- * _abs- print absolute value of an integer
- * @n: value to be compared
- * Return: int
-*/
+ * _abs - main function
+ *
+ * @n: integer to make absolute
+ * Return: int or int times negative
+ *
+ */
+
 int _abs(int n)
 {
-	n = abs(n);
-
-	return (n);
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
 }
