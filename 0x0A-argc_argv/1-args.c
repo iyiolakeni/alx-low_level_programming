88ff7b51954254da3aa9d953197c8a54bf974339
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-10 12:39:02 -0800

    Pass argument to print number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+*/
+int main(int argc, char **argv __attribute__ ((unused)))
+{
+	printf("%d\n", argc - 1);
+}
