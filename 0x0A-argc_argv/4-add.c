a56db6d87ad36f3e0c5a5f570bb3d5d29ae6142e
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-10 13:31:03 -0800

    changed

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+*/
+int main(int argc, char **argv)
+{
+	int add = 0;
+	int j, i;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]))
+			{
+				add += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", add);
+	return (0);
+}
