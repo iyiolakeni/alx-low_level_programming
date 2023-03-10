d3c1269e3741082b6ebfdb89d2b1fae62b5e16d9
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-10 13:48:06 -0800

    Another try

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,6 +10,7 @@
 int main(int argc, char  *argv[])
 {
 	int coins = 0;
+	int money;
 
 	if (argc == 2)
 	{
@@ -18,8 +19,6 @@ int main(int argc, char  *argv[])
 			printf("0\n");
 			return (1);
 		}
-		int money;
-
 		money = atoi(argv[argc - 1]);
 
 		while (money > 0)
