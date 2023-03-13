64b82435e578c59bd5323690e552e5d75816bbb9
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-13 11:36:59 -0700

    After several trials, removed the second loop and converted it to a while

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - function to return pointer to allocated mem space
+ * @str: character
+ *
+ * Return: 0 always
+*/
+char *_strdup(char *str)
+{
+	int i;
+	int len = 0; /* integer for pointer str counter */
+	char *mem;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (str[len] != '\0')
+			len++;
+	}
+
+	mem = malloc(sizeof(str) * len + 1);
+
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < len; i++)
+			mem[i] = str[i];
+	}
+	return (mem);
+}
