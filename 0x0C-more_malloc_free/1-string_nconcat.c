4ddd4aefcd759e52e21fe280f7851b189a240b74
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-16 04:26:05 -0700

    Replace if statement

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,38 +10,35 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0, lens1 = 0, lens2 = 0;
+	unsigned int i, lens1, lens2;
 	char *mem;
 
-	if (s1 == NULL)
-		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	if (s1 == NULL)
+		s1 = "";
 
+	lens1 = 0;
+	lens2 = 0;
+	while (s2[lens2] != '\0')
+		len2++;
 	while (s1[lens1] != '\0')
 		lens1++;
-	while (s2[lens2] != '\0')
-		lens2++;
 
 	if (n >= lens2)
 		n = lens2;
 
-	mem = malloc(sizeof(char) * (lens1 + n + 1));
-	if (mem != NULL)
-	{
-		for (i = 0; i < lens1; i++)
-		{
-			mem[i] = s1[i];
-		}
-		for (i = 0; i < n; i++)
-		{
-			mem[i + lens1] = s2[i];
-		}
-		mem[i + lens1] = '\0';
-		return (mem);
-	}
-	else
-	{
+	mem = malloc(sizeof(char) * n + lens1 + 1);
+	if (s == NULL)
 		return (NULL);
-	}
+
+	for (i = 0; i < lens1; i++)
+		s[i] = s1[i];
+
+	for (i = 0; i < n; i++)
+		mem[i + lens1] = s2[i];
+
+	mem[i + lens1] = '\0';
+
+	return (mem);
 }
