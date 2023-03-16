134309262b0f3883839ca4cc06e5e125686bd767
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-16 04:29:45 -0700

    Hope

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	lens1 = 0;
 	lens2 = 0;
 	while (s2[lens2] != '\0')
-		len2++;
+		lens2++;
 	while (s1[lens1] != '\0')
 		lens1++;
 
@@ -29,11 +29,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = lens2;
 
 	mem = malloc(sizeof(char) * n + lens1 + 1);
-	if (s == NULL)
+	if (mem == NULL)
 		return (NULL);
 
 	for (i = 0; i < lens1; i++)
-		s[i] = s1[i];
+		mem[i] = s1[i];
 
 	for (i = 0; i < n; i++)
 		mem[i + lens1] = s2[i];
