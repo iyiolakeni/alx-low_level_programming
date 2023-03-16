2bc15f69eef26344ad425fa56850b9eaafd5bd5a
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-16 04:35:12 -0700

    Changed a while loop to for

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,32 +10,26 @@
  */
 int *array_range(int min, int max)
 {
-	int i = 0;
+	int i;
 	int min_max = 0;/*A value to hold the length of number */
 	int *mem;
 
 	if (min > max)
 		return (NULL);
 
+	for (i = min; i <= max; i++)
+		min_max++;
+
+	mem = malloc(sizeof(int) * min_max);
+	if (mem == NULL)
+		return (NULL);
+
+	i = 0;
 	while (min <= max)
 	{
+		mem[i] = min;
+		i++;
 		min++;
-		min_max++;
 	}
-	mem = malloc(sizeof(int) * min_max);
-
-	if (mem != NULL)
-	{
-		while (min < max)
-		{
-			mem[i] = min;
-			i++;
-			min++;
-		}
-		return (mem);
-	}
-	else
-	{
-		return (NULL);
-	}
+	return (mem);
 }
