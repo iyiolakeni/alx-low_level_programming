19af303ab8a87e15bce5857ee2e3eb8378efdec7
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-03-22 05:57:18 -0700

    Requirement

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 		int (*cal)(int, int);
 
 		cal = get_op_func(argv[2]);
-		if ((*argv[2] == '%' || *argv[2] == '/') && (num1 == 0 || num2 == 0))
+		if ((*argv[2] == '%' || *argv[2] == '/') && num2 == 0)
 		{
 			printf("Error\n");
 			exit(100);
