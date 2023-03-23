b359318d893ddbd77efc6e191db533d9950b5534
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-03-23 22:06:41 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,14 +6,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
-/**
- * struct ag - struct
- * @letter: letter signifying data type
- * @func: function ptr
- */
-typedef struct ag
-{
-	char letter;
-	void (*func)(va_list);
-} arg;
 #endif
