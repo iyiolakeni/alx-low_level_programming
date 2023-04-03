b0e5e82e7123f00d87d61015ff6ec6fb3c9de848
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-04-03 05:51:39 -0700

    Print Element numbers

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+i#include "lists.h"
+
+/**
+ * add_nodeint - function to add a new node to the beginning of the list
+ * @head: an array and the head node
+ * @n: value of the node
+ * Return: return new nodes
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *mem;
+
+	mem = malloc(sizeof(listint_t));
+	if (mem == NULL)
+		return (NULL);
+
+	mem->n = n;
+	mem->next = *head;
+
+	*head = mem;
+
+	return (*head);
+}
