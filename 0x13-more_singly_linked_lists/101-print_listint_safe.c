86d41761564ae5b80f65e6c25643483038814c20
Iyioluwa Awe <iyiolakeni@gmail.com>
2023-04-04 14:50:51 -0700

    try

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,31 +1,97 @@
-#include "listint_t.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * count_nodes_till_loop - count nodes to know now many unique nodes to print
+ * @head: pointer to head pointer of linked list
+ * Return: number of unique nodes in list before a loop
+ */
+int count_nodes_till_loop(const listint_t *head)
+{
+	int count = 0;
+	const listint_t *turtle, *hare;
+
+	turtle = hare = head;
+
+	while (turtle != NULL && hare != NULL)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+		count++;
+
+		if (turtle == hare)
+		{
+			turtle = head;
+			while (turtle != hare)
+			{
+				turtle = turtle->next;
+				hare = hare->next;
+				count++;
+			}
+			return (count);
+		}
+	}
+	return (0);
+}
+
+/**
+ * loop - find if there's a loop in linked list
+ * @head: pointer to head pointer of linked list
+ * Return: 0 if no loop, 1 if loop
+ */
+int loop(const listint_t *head)
+{
+	const listint_t *turtle, *hare;
+
+	turtle = hare = head;
+
+	while (turtle != NULL && hare != NULL)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+
+		if (turtle == hare)
+			return (1);
+	}
+	return (0);
+}
+
 /**
  * print_listint_safe - prints list with addresses
  * @head: pointer to head pointer of linked list
  * Return: number of nodes in list, exit(98) if failed
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head;
-	const listint_t *fast = head;
-	size_t count = 0;
+	int count = 0;
+	int loop_found;
+	size_t num_nodes = 0;
+	const listint_t *tmp;
 
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	if (head == NULL)
+		exit(98);
+
+	loop_found = loop(head);
+
+	if (loop_found == 1) /* print upto last node before loop if loop */
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
+		count = count_nodes_till_loop(head);
+		for (loop_found = 0; loop_found < count; loop_found++)
 		{
-			printf("List contains a loop\n");
-			exit(98);
+			printf("[%p] %d\n", (void *)tmp, tmp->n);
+			num_nodes += 1;
+			tmp = tmp->next;
 		}
-
-		printf("%d\n", slow->n);
-		count++;
 	}
-	return (count);
+	else if (loop_found == 0) /* print regularly upto NULL if no loop */
+	{
+		tmp = head;
+		while (tmp != NULL)
+		{
+			printf("[%p] %d\n", (void *)tmp, tmp->n);
+			num_nodes += 1;
+			tmp = tmp->next;
+		}
+	}
+
+	return (num_nodes);
 }
